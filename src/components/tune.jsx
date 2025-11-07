49fabe5f067c8e0112625081b1f171fda342e903
Joakim Wennergren <joakim.wennergren@afry.com>
2025-11-07 10:25:35 +0100

    link

--- src/components/tune.jsx
+++ src/components/tune.jsx
@@ -41,7 +41,7 @@ export default function tune({ tune }) {
                         <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                             <li>
                                 <div className="flex items-center">
-                                    <a href="#" className="mr-2 text-sm font-medium text-gray-900">Hem</a>
+                                    <a href="/" className="mr-2 text-sm font-medium text-gray-900">Hem</a>
                                     <svg viewBox="0 0 16 20" width="16" height="20" fill="currentColor" aria-hidden="true" className="h-5 w-4 text-gray-300">
                                         <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
                                     </svg>
